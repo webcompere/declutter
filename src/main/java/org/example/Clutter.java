41187d5daac9796ea075e8f5f2296a6e6292a53c
Ashley Frieze <ashley.frieze@volo.ai>
2019-01-25 10:46:55 +0000

    Cascading ifs turned into AND

--- src/main/java/org/example/Clutter.java
+++ src/main/java/org/example/Clutter.java
@@ -7,14 +7,12 @@ public class Clutter {
     public static List<String> findStringsContaining(final List<String> source, final String subString) {
         final List<String> result = new ArrayList<>();
 
-        if (subString != null) {
-            if (!subString.isEmpty()) {
-                for (String item:source) {
-                    final String[] words = item.split(" ");
-                    for (String word:words) {
-                        if (word.compareToIgnoreCase(subString) == 0) {
-                            result.add(item);
-                        }
+        if (subString != null && !subString.isEmpty()) {
+            for (String item:source) {
+                final String[] words = item.split(" ");
+                for (String word:words) {
+                    if (word.compareToIgnoreCase(subString) == 0) {
+                        result.add(item);
                     }
                 }
             }
