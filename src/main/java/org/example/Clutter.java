4e17220fdc6541b6eaec1d38a99aca2111fc8bbe
Ashley Frieze <ashley.frieze@volo.ai>
2019-01-25 10:48:28 +0000

    Reverse guard condition and get out early

--- src/main/java/org/example/Clutter.java
+++ src/main/java/org/example/Clutter.java
@@ -3,21 +3,23 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
+
 public class Clutter {
     public static List<String> findStringsContaining(final List<String> source, final String subString) {
-        final List<String> result = new ArrayList<>();
+        if (subString == null || subString.isEmpty()) {
+            return emptyList();
+        }
 
-        if (subString != null && !subString.isEmpty()) {
-            for (String item:source) {
-                final String[] words = item.split(" ");
-                for (String word:words) {
-                    if (word.compareToIgnoreCase(subString) == 0) {
-                        result.add(item);
-                    }
+        final List<String> result = new ArrayList<>();
+        for (String item : source) {
+            final String[] words = item.split(" ");
+            for (String word : words) {
+                if (word.compareToIgnoreCase(subString) == 0) {
+                    result.add(item);
                 }
             }
         }
-
         return result;
     }
 }
